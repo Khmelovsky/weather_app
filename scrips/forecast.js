875cbaa1d7173935053b514d65913367ec9e8197
Andrii <A.I.Khmelovsky@gmail.com>
2019-08-20 14:21:41 +0300

    Api key added

---
+++ scrips/forecast.js
@@ -0,0 +1,17 @@
+const key = 'uPkAJMpp0Sdvv3Pjb6CWYAIR7aaaht28';
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query)
+
+    const data = await response.json()
+
+    return data[0];
+}
+getCity('Mukachevo')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
